33b7929eb520a4dcea22ddf56497dc659a99b741
longpiseth2002 <stevkhat121212@gmail.com>
2024-05-08 14:19:37 +0700

    New

---
+++ src/main/java/co/istad/istadlms/IstadLmsApplication.java
@@ -0,0 +1,13 @@
+package co.istad.istadlms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IstadLmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IstadLmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/istad/istadlms/IstadLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.istad.istadlms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IstadLmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
