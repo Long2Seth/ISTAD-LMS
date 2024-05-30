faa32fc5107beb703bcfddf23dc82d7a20c9ac0d
povsoknem <povsoknem@gmail.com>
2024-05-31 05:09:50 +0700

    Auto commit

--- src/main/java/co/istad/lms/features/score/dto/ScoreResponse.java
+++ src/main/java/co/istad/lms/features/score/dto/ScoreResponse.java
@@ -1,4 +1,16 @@
 package co.istad.lms.features.score.dto;
 
-public record ScoreResponse() {
+public record ScoreResponse(
+        String uuid,
+        Integer semester,
+        Double activityScore,
+        Double attendanceScore,
+        Double midtermExamScore,
+        Double finalExamScore,
+        Double miniProjectScore,
+        Double assignmentScore,
+        String studentAlias,
+        String courseAlias,
+        Boolean isDeleted
+) {
 }
