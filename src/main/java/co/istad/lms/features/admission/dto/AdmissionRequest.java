d79910697ec365e6df8a15223248097b457f4530
povsoknem <povsoknem@gmail.com>
2024-05-30 19:42:35 +0700

    Auto commit

--- src/main/java/co/istad/lms/features/admission/dto/AdmissionRequest.java
+++ src/main/java/co/istad/lms/features/admission/dto/AdmissionRequest.java
@@ -1,16 +1,16 @@
 package co.istad.lms.features.admission.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
+
 import java.time.LocalDate;
 
 // DTO for creating a new Admission
 public record AdmissionRequest(
-//        @NotNull(message = "status is require")
-//        @Pattern(regexp = "[1-3]", message = "status must be 1, 2, or 3")
+        @NotNull(message = "status is require")
+        @Min(value = 1, message = "Status must be 1, 2, or 3")
+        @Max(value = 3, message = "Status must be 1, 2, or 3")
         Integer status,
+
         String remark,
         @NotNull(message = "openDate is require")
         LocalDate openDate,
