d5cf2f8eb8d6f9502a5cc3d541043d418264c2ff
longpiseth2002 <stevkhat121212@gmail.com>
2024-06-27 14:56:23 +0700

    marge

--- src/main/java/co/istad/lms/features/student/dto/StudentResponse.java
+++ src/main/java/co/istad/lms/features/student/dto/StudentResponse.java
@@ -2,19 +2,83 @@ package co.istad.lms.features.student.dto;
 
 import co.istad.lms.features.course.dto.CourseResponse;
 import co.istad.lms.features.user.dto.UserResponse;
+import jakarta.persistence.ElementCollection;
+import jakarta.validation.constraints.*;
 
 import java.time.LocalDate;
 import java.util.Set;
 
 public record StudentResponse(
-        String uuid,
+
         String nameEn,
+
+
         String nameKh,
-        String username,
-        String email,
-        String phoneNumber,
-        LocalDate dob,
+
+
+
         String gender,
-        String profileImage
+
+
+
+        String email,
+
+
+
+        String phoneNumber,
+
+
+
+        Set< String> educations,
+
+
+
+
+        Set<String> skills,
+
+
+
+
+        String birthPlace,
+
+
+
+
+        String currentAddress,
+
+
+
+
+        String bio,
+
+
+
+        String dob,
+
+
+
+
+        String linkGit,
+
+
+
+
+        String linkLinkedin,
+
+
+
+
+        String linkTelegram,
+
+
+
+
+
+        String uploadCv,
+
+
+
+
+        String identityCard
 ) {
 }
